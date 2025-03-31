bceca45a4bc84521aa4fcc37f5f701fa69be82d7
rajakanjali30 <129724538+rajakanjali30@users.noreply.github.com>
2025-03-31 15:16:43 +0530

    Add tailwind.config.js 1743414403881

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3B82F6',
+        secondary: '#161E2E',
+      },
+    },
+  },
+  plugins: [],
+}
